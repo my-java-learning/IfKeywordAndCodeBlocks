7f7172db48e3fc2f004a6f5a59af0808f6b88df0
Qsior <lukaszbojes@gmail.com>
2017-10-13 17:24:48 +0200

    Scope variable example

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -7,15 +7,19 @@ public class  Main {
         int score = 5000;
         int levelCompleted = 5;
         int bonus = 100;
-        if(score < 5000 && score > 1000) {
-            System.out.println("Your score is less than 5000");
-        } else if(score < 1000){
-            System.out.println("Your score is less than 1000");
-        }else {
-            System.out.println("Got here");
+
+//        if(score < 5000 && score > 1000) {
+//            System.out.println("Your score is less than 5000");
+//        } else if(score < 1000){
+//            System.out.println("Your score is less than 1000");
+//        }else {
+//            System.out.println("Got here");
+//        }
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + finalScore);
         }
-
-
 
     }
 }
