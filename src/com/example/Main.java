1dcadaa22422adf13cadd391cab519c30eba4173
Qsior <lukaszbojes@gmail.com>
2017-10-13 17:19:11 +0200

    if else examples

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,19 @@ package com.example;
 public class  Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+        if(score < 5000 && score > 1000) {
+            System.out.println("Your score is less than 5000");
+        } else if(score < 1000){
+            System.out.println("Your score is less than 1000");
+        }else {
+            System.out.println("Got here");
+        }
+
+
+
     }
 }
